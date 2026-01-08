8d959adf1c2c45a6cb92781379526116b061b8d1
willliamcamasil <williamcamargosilva@gmail.com>
2026-01-08 14:45:53 -0300

    feat: adiciona lib axios para chamada GET

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { api } from "./repositories/api/api";
 import "./App.css";
 
 const App = () => {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const handleSetUsers = () => {
+    setLoading(true);
+    api
+      .get("/users")
+      .then((res) => setUsers(res.data), setLoading(false))
+      .catch((err) => console.error(err), setLoading(false));
+  };
+
+  useEffect(() => {
+    handleSetUsers();
+  }, []);
+
+  const renderApiWithNames = () => {
+    if (loading) return <p>Carregando dados</p>;
+    if (!users) return <p>Dados não encontrados</p>;
+
+    return (
+      <div>
+        <p>Dados da API: </p>
+        {users.map((user) => (
+          <p key={user.id}>
+            {user.name} - {user.email}
+          </p>
+        ))}
+      </div>
+    );
+  };
+
   return (
     <div className="dashboard">
       <p className="dashboard-p">Dashboard - MFE</p>
+      {renderApiWithNames()}
     </div>
   );
 };
---
+++ src/repositories/api/api.js
@@ -0,0 +1,6 @@
+// api.js
+import axios from "axios";
+
+export const api = axios.create({
+  baseURL: "https://jsonplaceholder.typicode.com",
+});
